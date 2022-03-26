dbda531b02c28edf7321d8f63bcb624474ae032b
Ad0rian <justisatroubleday@gmail.com>
2022-03-26 16:49:19 +0100

    Punto 4

--- bd/data_json.js
+++ bd/data_json.js
@@ -1,17 +1,37 @@
 //JSON FILES
-var datahotels =
+var data =
 {
-    "hotels": [
+    "hotels": [ 
         {
-            "code": "hotel_code",
-            "name": "hotel_name",
-            "city": "hotel_city",
-            "rooms": {
-                "name": "room_name",
-                "room_type": "room_type(Enum)",
+            "code": "0",
+            "name": "Atalaya",
+            "city": "Cancún",
+            "rooms": [{
+                "name": "Room 102",
+                "room_type": "Two person Room",
+                "meals_plan" :"No meals",
+                "price": "437.5€",
+                "nights": "Five nights"
+                },
+                {
+                "name": "Room 103",
+                "room_type": "room_tpye(Enum)",
                 "meals_plan" :"meal_code(Enum)",
-                "price": "price_per_night_per_person"
-                }
+                "price": "total_price",
+                "nights": "nights"
+                    }]
+        },
+        {
+            "code": "1",
+            "name": "Resort Hotels",
+            "city": "Málaga",
+            "rooms": [{
+                "name": "Room 202",
+                "room_type": "Two person Room",
+                "meals_plan" :"Meals included",
+                "price": "262.5€",
+                "nights": "Three nights"
+                }]
         }
     ]
 };
--- src/content.js
+++ src/content.js
@@ -1,21 +1,20 @@
 var hotels ='http://www.mocky.io/v2/5e4a7e4f2f00005d0097d253';
-var rooms ='https://run.mocky.io/v3/132af02e-8beb-438f-ac6e-a9902bc67036';
-
+var meals ='http://www.mocky.io/v2/5e4a7e282f0000490097d252';
 $.when(
     $.getJSON(hotels ),
-    $.getJSON(rooms )
-    ).done(function(datahotels, datarooms) {
+    $.getJSON(meals )
+    ).done(function(datahotels, datameals) {
         console.log(datahotels);
-        console.log(datarooms);
-        initialPage(datahotels,datarooms);
+        console.log(datameals);
+        initialPage(datahotels,datameals);
     });
 
-function initialPage(datahotels,datarooms){
+function initialPage(datahotels,datameals){
     $('#container_hotels').html('');
     var hotelslist = datahotels[0].hotels; //get DATA
-    var roomslist = datarooms[0].rooms_type; //get DATA
+    var mealslist = datameals[0].meal_plans; //get DATA
     console.log(datahotels);
-    console.log(roomslist);
+    console.log(datameals);
     for (var i = 0; i < hotelslist.length; i++){
      $('#container_hotels').append(
         '<div id="'+hotelslist[i].code+'" class="sizehotelClass">'+
@@ -27,30 +26,35 @@ function initialPage(datahotels,datarooms){
             '<div id="city_hotel" class="texthotelClass bfontClass">'+hotelslist[i].city+'</div>'+
         '</div>'+
         '<div id="line_sep" class="lineClass"></div>'+
-            '<div id="hotelcontainer_'+hotelslist[i].code+'" class="gridhotelClass">    '         +
+            '<div id="hotelcontainer_'+hotelslist[i].code+'" class="gridhotelClass">'+
             '</div>'+
     '<div id="code_hotel" class="codehotelClass">'+hotelslist[i].code+'</div>'  +
     '</div>'
         );
     }
-    for (var i = 0; i < roomslist.length; i++){
-        for (var j = 0; j < roomslist[i].hotels.length; j++){
-            console.log(roomslist[i].hotels[j]);
-        $('#hotelcontainer_'+roomslist[i].hotels[j]+'').append(
+    for (var i = 0; i < mealslist.length; i++){
+       // var mealsputlength = Object.keys(mealslist[i].hotel).length;
+        var mealsput = Object.keys(mealslist[i].hotel);
+        for (var j = 0; j < mealsput.length; j++){
+
+        for (var z = 0; z < mealslist[i].hotel[mealsput[j]].length; z++){
+            var meals =mealslist[i].hotel[mealsput[j]][z];
+            console.log(meals);
+        $('#hotelcontainer_'+mealsput[j]+'').append(
             '<div id="room_hotel" class="roomhotelClass">'+
-            '<div id="room_name" class="texthotelClass h2fontClass clClass">'+roomslist[i].name+'</div>'+
+            '<div id="'+meals.room+'" class="texthotelClass h2fontClass clClass">'+meals.room+'</div>'+
             '<div id="line_sep" class="lineClass clClass"></div> '+
-           '<div id="type_room" class="texthotelClass b2fontClass ltClass clClass">'+roomslist[i].code+'</div>'+
+           '<div id="type_room" class="texthotelClass b2fontClass ltClass clClass">'+meals.room+'</div>'+
             '<div id="line_sep" class="linetinyClass clClass"></div>'+
-            '<div id="meals_room" class="texthotelClass b2fontClass ltClass clClass">meals_room</div>'+
+            '<div id="'+mealslist[i].code+'" class="texthotelClass b2fontClass ltClass clClass">'+mealslist[i].name+'</div>'+
             '<div id="line_sep" class="linetinyClass clClass"></div>'+
-            '<div id="price_room" class="texthotelClass b2fontClass ltClass clClass">price_room</div>'+
+            '<div id="price_room" class="texthotelClass b2fontClass ltClass clClass">'+meals.price+'</div>'+
             '<div id="line_sep" class="linetinyClass clClass"></div>'+
             '<div id="nights_room" class="texthotelClass b2fontClass ltClass clClass">nights_room</div>'+
         '</div>'+
        '</div>'
            );
-        
+        }
         }
        }
 
