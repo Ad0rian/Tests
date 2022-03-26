c25e51fb5b0c441b4abb862c7d6932d1f99762bf
Ad0rian <justisatroubleday@gmail.com>
2022-03-26 13:37:29 +0100

    Punto3

--- src/content.js
+++ src/content.js
@@ -1,34 +1,28 @@
 var staticUrl = 'http://www.mocky.io/v2/5e4a7e4f2f00005d0097d253';
 $.getJSON(staticUrl,function(data){
     console.log(data);
-    initialPage(configinterface.initialpage,data);
+    initialPage(data);
 });
 
-function initialPage(pageinitial,data){
+function initialPage(data){
+    $('#container_hotels').html('');
     var hotelslist = data.hotels; //get DATA
     console.log(hotelslist);
     for (var i = 0; i < hotelslist.length; i++){
-        
-     $('#lateralmenu').append(
-        '<div class="buttonmenu">'+
-        '<div id="'+hotelslist.i+'" class="buttonmenusecond" onclick="internalMenu('+hotelslist.i+');">'+                             
-            '<p class="textbutton">'+hotelslist[i].name+'</p>'+
+     $('#container_hotels').append(
+        '<div id="'+hotelslist[i].code+'" class="sizehotelClass">'+
+        '<div id="header_hotel" class="flexhotelClass">'+
+            '<div id="name_hotel" class="texthotelClass hfontClass">'+hotelslist[i].name+'</div>  '+
         '</div>'+
+    '<div id="line_sep" class="lineClass"></div>'+
+        '<div id="place_hotel" class="flexhotelClass">'+
+            '<div id="city_hotel" class="texthotelClass bfontClass">'+hotelslist[i].city+'</div>'+
+        '</div>'+
+        '<div id="line_sep" class="lineClass"></div>'+
+            '<div id="hotelcontainer" class="gridhotelClass">    '         +
+            '</div>'+
+    '<div id="code_hotel" class="codehotelClass">'+hotelslist[i].code+'</div>'  +
     '</div>'
         );
     }
-    $('#contentprincipal').append(
-        '<div id="buttonmenuside" class="buttonnavbar animationmenunavbar">'+
-        '<button class="hiddenbtnmenu" onclick="closeNav()"><</button>'+
-        '</div>'
-        );
-    $('#contentprincipal').append(
-    '<div id="lateralcontainer" class="sizerightbig animationiniright">'+
-    '<div id="contentRight" class="divmenusecondright">'+
-    '</div>'+
-    '</div>'
-    );
-    //var prevButton = pageinitial;
-    //internalContent(pageinitial,idButtoneffect);
-    internalContent(pageinitial);		
 }
