84e8632479fa30dae97a77eb68ba0dcd93da662c
Ad0rian <justisatroubleday@gmail.com>
2022-03-26 16:49:32 +0100

    Punto 5

--- src/content.js
+++ src/content.js
@@ -21,7 +21,7 @@ function initialPage(datahotels,datameals){
         '<div id="header_hotel" class="flexhotelClass">'+
             '<div id="name_hotel" class="texthotelClass hfontClass">'+hotelslist[i].name+'</div>  '+
         '</div>'+
-    '<div id="line_sep" class="lineClass"></div>'+
+    '<div id="line_sep" class="lineClass"></div>'+ 
         '<div id="place_hotel" class="flexhotelClass">'+
             '<div id="city_hotel" class="texthotelClass bfontClass">'+hotelslist[i].city+'</div>'+
         '</div>'+
